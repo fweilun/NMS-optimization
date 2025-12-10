9b9708b95778c08b3f5a8745fc2b88058d21a0fe
weilun <weilun@weiluns-MacBook-Air.local>
2025-12-10 22:44:53 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+ml cuda
+
 EXEC=$1
 shift
+make $EXEC
 
 echo "===== Warm Up ====="
 ./"$EXEC" "$@"
 
 echo
 echo "===== Profiling ====="
-nvprof ./"$EXEC" "$@"
+
+srun -p nvidia -N1 -n1 --gres=gpu:1 nvprof --metrics achieved_occupancy,sm_efficiency,shared_load_throughput,shared_store_throughput,gld_throughput,gst_throughput \
+ --log-file profiles/profiling_metrics-\($EXEC-$@\).log ./"$EXEC" "$@"
 
 srun -p nvidia -N1 -n1 --gres=gpu:1 nvprof --print-gpu-summary --log-file time_distribution.log \
---metrics achieved_occupancy,sm_efficiency,shared_load_throughput,shared_store_throughput,gld_throughput,gst_throughput \
-./"$EXEC" "$@"
+--log-file profiles/time_distribution-\($EXEC-$@\).log ./"$EXEC" "$@"
